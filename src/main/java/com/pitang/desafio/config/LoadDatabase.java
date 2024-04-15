9ca65ecba246d8611f517ad5d4d29bb3dd1e1543
jadsonti <31677332+jadsonti@users.noreply.github.com>
2024-04-15 02:10:44 -0300

    fix: config
    corrige criptografia de senha em insert de dados inicial da aplicação.

--- src/main/java/com/pitang/desafio/config/LoadDatabase.java
+++ src/main/java/com/pitang/desafio/config/LoadDatabase.java
@@ -7,20 +7,30 @@ import com.pitang.desafio.model.User;
 import com.pitang.desafio.model.Car;
 import com.pitang.desafio.repository.UserRepository;
 import com.pitang.desafio.repository.CarRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 @Configuration
 public class LoadDatabase {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public LoadDatabase(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Bean
     CommandLineRunner initDatabase(UserRepository userRepository, CarRepository carRepository) {
         return args -> {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date dateAlice = sdf.parse("1990-01-01");
+            Date dateBob = sdf.parse("1992-02-02");
 
-            User alice = new User(null, "Alice", "Smith", "alice.smith@example.com", sdf.parse("1990-01-01"), "alicesmith", "password1", "1234567890");
-            User bob = new User(null, "Bob", "Johnson", "bob.johnson@example.com", sdf.parse("1992-02-02"), "bobjohnson", "password2", "0987654321");
+            User alice = new User(null, "Alice", "Smith", "alice.smith@example.com", dateAlice, "alicesmith", passwordEncoder.encode("password1"), "1234567890");
+            User bob = new User(null, "Bob", "Johnson", "bob.johnson@example.com", dateBob, "bobjohnson", passwordEncoder.encode("password2"), "0987654321");
 
             userRepository.save(alice);
             userRepository.save(bob);
